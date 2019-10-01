701612d6cb0e820c15ec9fe7d5eee97790ab288d
wictorgirardi <wictor.girardi@gmail.com>
2019-10-01 01:09:07 -0300

    Adicionando algoritmos

--- lista3.py
+++ lista3.py
@@ -3,6 +3,63 @@ import random
 import numpy as np
 from threading import Thread
 
+#ordenation funcions
+
+def selection_sort(img_tuple):
+    size = len(img_tuple)
+    for i in range(size):
+        for j in range(i+1, size):
+            if img_tuple[j][0] < img_tuple[i][0]:
+                aux = img_tuple[i]
+                img_tuple[i] = img_tuple[j]
+                img_tuple[j] = aux
+
+    return img_tuple
+
+def bubble_sort(img_tuple):
+    #subtraimos por um para sempre compararmos com os proximos valores
+    size = len(img_tuple) - 1
+    #realiza o calculo de quantas vezes vai ser necessaria
+    #a passagem pelo tamanho do vetor
+    for i in range(size):
+        #realiza a troca de posicoes dentro dos vetores
+        for j in range(size -i):
+            if img_tuple[j][0] > img_tuple[j+1][0]:
+                img_tuple[j], img_tuple[j+1] = img_tuple[j+1], img_tuple[j]
+    return img_tuple
+
+def shell_sort(img_tuple):
+ size = len(img_tuple)
+ gap = int(size/2)
+ while(gap >= 1):
+  i = gap
+  while(i < size):
+   value = img_tuple[i]
+   j = i
+   while(j-gap >= 0 and value < img_tuple[j - gap]):
+    img_tuple[j] =  img_tuple[j - gap]
+    j -= gap
+   img_tuple[j] = value
+   i+=1
+  gap = int(gap/2)
+ return img_tuple
+
+# Como se a cada elemento encontrado, o algorítmo percorre o vetor do fim pro começo para encontrar a posição adequada de inserção
+def insertion_sort(img_tuple):
+    size = len(img_tuple)
+    for index in range(1,size): # Considering that the fist element is in the right position
+        current = img_tuple[index][0] #Stablish fixed element for comparison [(**id**, RGB)]
+        current_tuple = img_tuple[index] # [**(id, RGB)**]
+        position = index
+        #comparing one element to the one that is behind the current element
+        while  position > 0 and img_tuple[position-1][0] > current:
+           # Doing the swaping
+           img_tuple[position] = img_tuple[position-1]
+           position -= 1
+        img_tuple[position] = current_tuple #Tuple being swapped from current_tuple
+    # return the sorted tupl
+    return img_tuple
+
 def convert_image(img_tuple, lines, columns):
     new_image = np.empty([lines, columns, 3], dtype="uint8")
     for i in range(lines):
